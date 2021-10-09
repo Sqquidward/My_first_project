fc8ebbc248c7c5b81233647d52e606dc2c9145d7
Sqquidward <92225118+Sqquidward@users.noreply.github.com>
2021-10-10 00:37:54 +0300

    Update Task_1.java

--- My_first_project/src/com/company/Task_1.java
+++ My_first_project/src/com/company/Task_1.java
@@ -2,17 +2,18 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Task_1 {
+public class task_2 {
     public static void main(String[] args){
+        sum();
+    }
+
+
+    public static int sum(){
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
         int c = Math.max(a, b);
-        bol(c);
-
-    }
-    static void bol(int c){
         System.out.println(c);
-
+        return c;
     }
 }
